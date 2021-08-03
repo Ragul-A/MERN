036acf61e12b0712fb953c3de92e7325524f626e
Ragul-A <ragul.angamuthu@oracle.com>
2021-08-03 13:58:40 +0530

    mongo connection post working

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
 import config from './config';
 import express from 'express';
 const cors = require('cors');
 const server = express();
 // const mongoose = require('mongoose');
-// const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, {useUnifiedTopology: true});
 
 server.use(cors());
 server.use(express.json());
@@ -11,11 +14,36 @@ server.use(express.json());
 // const URI = process.env.ATLAS_URI;
 
 // mongoose.conn
+
+const db = client.db('registeration');
+// MongoClient.connect('mongodb-connection-string', (err, client) => {
+//   // ... do something here
+// });
+
 server.listen(config.port, () => {
   console.log('Server listening on PORT', config.port);
 });
 
 server.post('/register', function(req, res) {
-  console.log('req', req);
+  // db.collection('userData').insertOne(req.body);
+  console.log('req', req.body);
   console.log('res', res);
+  const client = new MongoClient('mongodb://localhost:27017');
+  run();
+  async function run() {
+    try {
+      await client.connect();
+      const database = client.db('registeration');
+      const collection = database.collection('userData');
+      console.log('collection', collection);
+
+      const result = await collection.insertOne(req.body);
+      res.end(JSON.stringify(result));
+    } catch (e) {
+      console.log('Error: ' + e);
+    } finally {
+      await client.close();
+    }
+  }
+
 });
